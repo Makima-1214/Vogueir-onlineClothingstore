e069ee9f93ce1f5079bed2e545f69ecaf16b4d75
Makima-1214 <venom232013@gmail.com>
2026-06-11 21:12:15 +0700

    feat: update database configuration to use environment variable for DATABASE_URL

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,7 +1,7 @@
 export default {
   datasources: {
     db: {
-      url: 'postgresql://neondb_owner:npg_zvKOm2Enesu0@ep-royal-lake-aoj77e3w-pooler.c-2.ap-southeast-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+      url: process.env.DATABASE_URL,
     },
   },
 };
